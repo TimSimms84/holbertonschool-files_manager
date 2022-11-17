7df9b81cfc5b010cec507b429d2efdd43159a1cb
Tim Simms <tsimms@gmail.com>
2022-11-17 15:01:40 -0600

    task 5 works locally

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const AppController = require('../controllers/AppController');
 const UsersController = require('../controllers/UsersController');
 const AuthController = require('../controllers/AuthController')
+const FilesController = require('../controllers/FilesController.js')
 
+// Create a new router 
 const router = express.Router();
-
+// GETs
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
-
+// POSTs
 router.post('/users', UsersController.postNew);
+router.post('/files', FilesController.postUpload);
 
 module.exports = router;
